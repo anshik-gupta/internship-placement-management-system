8db2b06246f24be34c5a716ca4b1ab008b78e20d
Anshik Gupta <anshikgupta108@gmail.com>
2026-08-20 01:04:16 +0530

    Set up project structure and database connection

---
+++ src/app.js
@@ -0,0 +1,23 @@
+//core modules
+const express = require("express");
+const path = require("path");
+const db = require("./config/db");
+
+//user modules
+const userRouter = require("./routes/userRouter");
+const companyRouter = require("./routes/companyRouter");
+
+const app= express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/user", userRouter);
+app.use("/company", companyRouter)
+
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} `);
+});
